3de4d9df4a41a0163757ac1744d53c3c74e26801
Brian Jackson <decaelus@gmail.com>
2021-09-04 14:25:21 -0600

    Initial complete calculation

--- docs/examples.rst
+++ docs/examples.rst
@@ -7,4 +7,4 @@ Below is a collection of examples on how to use ``Mars Clock``.
    :titlesonly:
    :maxdepth: 1
 
-   Basic Usage <examples/Using_Mars_Clock.ipynb>
+   Using Ls_utils <examples/Using_Ls_utils.ipynb>
--- mars_clock/__init__.py
+++ mars_clock/__init__.py
@@ -2,6 +2,7 @@
 
 __version__ = '0.0.0'
 __author__ = 'Brian Jackson <bjackson@boisestate.edu>'
-__all__ = ['Ls_utils']
+__all__ = ['Ls_utils', 'mission_sols']
 
 from .Ls_utils import *
+from .mission_sols import *
